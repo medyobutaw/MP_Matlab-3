9f060898e6aa3091bb82baf276e2bd6a938c5a45
Joshua <57347039+medyobutaw@users.noreply.github.com>
2019-12-08 11:06:11 +0800

    Create matlab3.m

---
+++ matlab3.m
@@ -0,0 +1,23 @@
+function matlab3(e)
+    
+e1 = e(:,1);
+e2 = e(:,2);
+
+if length(e1) >= 11
+    l = 10;
+else
+    l = length(e1)-1;
+end
+    
+for L = 1:l
+    
+q = polyfit(e1,e2,L);
+t = polyval(q,e1);
+p(L) = norm(e2 - t);
+
+end
+
+[~,I] = min(p);
+c = polyfit(e1,e2,I);
+
+disp(c)
